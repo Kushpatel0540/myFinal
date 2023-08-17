4784af9b6576d8748982633fc7b427125bb25302
Kush Patel <kpatidar0540@gmail.com>
2023-08-17 09:45:13 -0400

    js file made

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const app = express();
+
+const port = process.env.PORT || 3050;
+const dbURI = 'mongodb://localhost:27017/bookstore'; 
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+let db;
+
+MongoClient.connect(dbURI, { useUnifiedTopology: true })
+  .then(client => {
+    db = client.db('bookstore');
+    console.log('Connected to database');
+  })
+  .catch(error => console.error(error));
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
+
+
+app.get('/', (req, res) => {
+    res.render('order-form');
+  });
+  
+  app.post('/place-order', (req, res) => {
+    const { name, studentId, items } = req.body;
+  
+    
+  
+    res.render('order-confirmation', { orderDetails });
+  });
+  
+  app.get('/view-orders', (req, res) => {
+    
+    res.render('view-orders', { orders });
+  });
+  
