345f603f616657e98805dd4f3fdf42c5553ca11c
Kush Patel <kpatidar0540@gmail.com>
2023-08-17 09:48:39 -0400

    localhost changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 const app = express();
 
-const port = process.env.PORT || 3050;
+const port = process.env.PORT || 3080;
 const dbURI = 'mongodb://localhost:27017/bookstore'; 
 
 app.set('view engine', 'ejs');
