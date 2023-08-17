ee54cc8793a804d01ae3e738a945ca33028a1d6d
Kush Patel <kpatidar0540@gmail.com>
2023-08-17 09:46:49 -0400

    another js file added

---
+++ db.js
@@ -0,0 +1,13 @@
+const getOrders = async () => {
+    // Retrieve orders from the database
+  };
+  
+  const insertOrder = async (order) => {
+    // Insert an order into the database
+  };
+  
+  module.exports = {
+    getOrders,
+    insertOrder,
+  };
+  
